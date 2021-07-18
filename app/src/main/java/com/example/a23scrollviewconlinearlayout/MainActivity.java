170c59e3cd6627e7ea02898daf646d2e99341d3c
MIGUEL BEASCOA <77923795+mbeascoa@users.noreply.github.com>
2021-07-18 08:27:33 +0200

    Initial commit

---
+++ app/src/main/java/com/example/a23scrollviewconlinearlayout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a23scrollviewconlinearlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
